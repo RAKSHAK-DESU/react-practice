54c4ec0a0ac66f659ddb37342b009dc3bd50c695
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-12-14 14:15:17 +0530

    added

---
+++ library/index.js
@@ -0,0 +1,12 @@
+var username="Ray Ellison";
+
+export function Hello(){
+    return `Hello ! ${username}`;
+}
+
+export class Product
+{
+    print(){
+        Print("Hello This is product");
+    }
+}
